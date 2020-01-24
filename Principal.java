8a43ddc7423039fb5379dddd72d2aa7cfb4031cb
hvassard <hugovassard04@gmail.com>
2020-01-24 15:26:44 -0500

    changement 100 à 200

--- Principal.java
+++ Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-        for(int i = 1 ; i < 101 ; i++ ){
+        for(int i = 1 ; i < 201 ; i++ ){
             if ( i%3 == 0 && i%5==0 ){
                 System.out.println("FizzBuzz");
             } else if ( i%3 == 0 ){
